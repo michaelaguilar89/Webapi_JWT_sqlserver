43e1ac43903d0ec8269269b898a2197992651399
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-22 15:23:50 -0600

    adding Dbset by products,users on Context class

--- Connection/Context.cs
+++ Connection/Context.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApi_JWT.Connection
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_JWT.Models;
+
+namespace WebApi_JWT.Connection
 {
-	public class Context
+	public class Context : DbContext
 	{
+		public Context(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Users> users { get; set; }
+
+		public DbSet<Products> products { get; set; }
 	}
 }
