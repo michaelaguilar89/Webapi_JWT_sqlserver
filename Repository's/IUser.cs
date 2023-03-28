48710b3d0eebde112bfb1ae429490b72617a4269
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-27 21:28:42 -0600

    adding Register,UserExist method on userRespository

--- Repository's/IUser.cs
+++ Repository's/IUser.cs
@@ -4,12 +4,12 @@ namespace WebApi_JWT.Repository_s
 {
 	public interface IUser
 	{
-		Task<string> Login(UserLogin user_login);
+	    Task<string> Login(UserLogin user_login);
 
 		Task<string> Register(Users user);
 
 		Task<string> Update(UserLogin user_login);
 
-		Task<bool> UserExist(UserLogin user_login);
+		Task<string> UserExist(string userName);
 	}
 }
---
+++ Repository's/User_Repository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_JWT.Connection;
+using WebApi_JWT.Models;
+
+namespace WebApi_JWT.Repository_s
+{
+	public class User_Repository : IUser
+	{
+		private readonly Context _db;
+		public User_Repository(Context db)
+		{
+			_db = db;
+		}
+
+		public async Task<string> Login(UserLogin user_login)
+		{
+			try
+			{
+			 var user= await _db.users.AnyAsync(x=>x.UserName == user_login.UserName);
+				if (user==null)
+				{
+					return "userName not Found";
+				}
+			}
+			catch (Exception)
+			{
+
+				return "-500";
+			}
+		}
+
+		public async Task<string> Register(UserLogin user,string rol)
+		{
+			try
+			{
+				string userExiste = await UserExist(user.UserName);
+				switch (userExiste)
+				{
+					case "exist!":
+						return "user exist";
+						
+
+					case "notExist":
+						break;
+
+						default:
+						break;
+				}
+
+
+				//creating encrypting password and key
+				CreatePasswordHash(user.Password, out byte[] passwordHash, out byte[] pasawordSalt);
+				Users newUser = new();
+				newUser.PasswordSalt = pasawordSalt;
+				newUser.PasswordHash = passwordHash;
+				newUser.UserName = user.UserName;
+				newUser.Rol= rol;
+				await _db.users.AddAsync(newUser);
+				await _db.SaveChangesAsync();
+				return "newUser : " + user.UserName;
+			}
+			catch (Exception)
+			{
+
+				return "-500";
+			}
+		}
+
+		private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] pasawordSalt)
+		{
+
+			using (var hmac = new System.Security.Cryptography.HMACSHA512())
+			{
+				pasawordSalt = hmac.Key;
+				passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+			}
+
+		}
+		public Task<string> Update(UserLogin user_login)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<string> UserExist(string userName)
+		{
+				if (await _db.users.AnyAsync(x=>x.UserName.ToLower().Equals(userName)))
+				{
+					return "exist!";
+				}
+				return "notExist";
+			
+			
+		}
+	}
+}
