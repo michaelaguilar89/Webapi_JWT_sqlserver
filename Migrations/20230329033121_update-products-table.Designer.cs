7788975acb772b8088c7d8dab73ba3f0c5822ae5
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-28 21:33:00 -0600

    adding migration update-database-products

--- Migrations/20230329033121_update-products-table.Designer.cs
+++ Migrations/20230329033121_update-products-table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi_JWT.Connection;
 
@@ -11,9 +12,10 @@ using WebApi_JWT.Connection;
 namespace WebApi_JWT.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230329033121_update-products-table")]
+    partial class updateproductstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
