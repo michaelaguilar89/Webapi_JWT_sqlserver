3c5db5689e64dcf3c3fc577e8db605d6817bba3d
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-28 22:31:54 -0600

    adding getAllProducts StoreProcedure on ProductsReposirory

--- Repository's/ProductsRepository.cs
+++ Repository's/ProductsRepository.cs
@@ -1,9 +1,16 @@
-﻿using WebApi_JWT.Models;
+﻿using Microsoft.Data.SqlClient;
+using WebApi_JWT.Models;
 
 namespace WebApi_JWT.Repository_s
 {
 	public class ProductsRepository : IProducts
 	{
+		private readonly string _connectionString;
+		public ProductsRepository(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
 		public Task<string> CreateUpdate(Products product)
 		{
 			throw new NotImplementedException();
@@ -19,9 +26,45 @@ namespace WebApi_JWT.Repository_s
 			throw new NotImplementedException();
 		}
 
-		public Task<List<Products>> GetProducts()
+		public async Task<List<Products>> GetProducts()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				using (SqlConnection sql = new SqlConnection(_connectionString))
+				{
+					using (SqlCommand cmd = new SqlCommand("getAllProducts",sql))
+					{
+						cmd.CommandType = System.Data.CommandType.StoredProcedure;
+						var response = new List<Products>();
+						await sql.OpenAsync();
+						using (var reader = await cmd.ExecuteReaderAsync())
+						{
+							while (await reader.ReadAsync())
+							{
+								response.Add(MapToValue(reader));
+							}
+						}
+						return response;
+					}
+				}
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+		private Products MapToValue(SqlDataReader reader)
+		{
+			return new Products()
+			{
+				Id = (int)reader["Id"],
+				Name = (string)reader["Name"],
+				Category = (string)reader["Category"],
+				Price = (decimal)reader["Price"],
+				Stock = (int)reader["Stock"]
+			};
 		}
 	}
 }
