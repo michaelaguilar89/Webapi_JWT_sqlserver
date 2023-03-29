ca22d002ae67974827dbf613c3a830b27bc11d7b
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-28 22:19:03 -0600

    implement interface IProducts on ProductsRepository

--- Repository's/ProductsRepository.cs
+++ Repository's/ProductsRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace WebApi_JWT.Repository_s
+﻿using WebApi_JWT.Models;
+
+namespace WebApi_JWT.Repository_s
 {
-	public class ProductsRepository
+	public class ProductsRepository : IProducts
 	{
+		public Task<string> CreateUpdate(Products product)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Products> GetProductById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Products>> GetProducts()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
